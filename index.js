72c7cdecc90fe06fa843147e8fa9d13c1a7afc9b
Patrick Kernan <pkernan18@yahoo.com>
2025-03-21 20:12:33 -0400

    Added localhost1234 to allowed origins

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const express = require('express'),
 
 const app = express();
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {

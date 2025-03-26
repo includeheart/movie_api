a21805e03a48865ea0cb425d6ac9cb90250d0891
Patrick Kernan <pkernan18@yahoo.com>
2025-03-25 21:37:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,15 +55,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to my movie site!');
 });
   
-app.get('/movies', async (req, res) => {
-    await Movies.find()
-    .then((movies) => {
-        res.status(201).json(movies);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-    });
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find()
+  .then((movies) => {
+      res.status(201).json(movies);
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+  });
 });
 
 app.get('/movies/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
